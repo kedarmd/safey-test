b6e98120a21cefe64e5dcdc2e40cb3afdfcce0fc
KMD <dindekedar@gmail.com>
2021-02-11 19:35:30 +0530

    setting monggose debug true

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ mongoose.connect(`mongodb://${process.env.HOST}/${process.env.DB}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
-
+mongoose.set('debug', true);
 mongoose.connection
     .once('open', () => console.log('connected to database...'))
     .on('error', (error) => {
